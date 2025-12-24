1d5cdfb6e4b125f92cf679cad3be87b550d77aff
johndoe6345789 <john.doe6345789@gmail.com>
2025-12-24 17:45:43 +0000

    Add CMake build with Conan and Ninja

---
+++ include/typthon/runtime.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stdio.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const char *typthon_version(void);
+void typthon_print_banner(FILE *output);
+
+#ifdef __cplusplus
+}
+#endif
+
